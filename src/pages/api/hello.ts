bae1178894dbec5a53d036d68e9cd2c98c2bd618
belunatic <abelluvyou@yahoo.com>
2025-03-22 15:08:24 -0700

    Remove unused SVG files and update API handler to return user data

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler() {
+type User = {
+	name: {
+		title: string;
+		first: string;
+		last: string;
+	};
+	email: string;
+	city: string;
+};
+
+export default async function handler(): Promise<User[] | undefined> {
 	try {
 		const response = await fetch("https://randomuser.me/api/?results=1");
 		const data = await response.json();
-		return data;
+		return data.results;
 	} catch (error) {
 		console.log(error);
 	}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,57 @@
+import { useState, useEffect } from "react";
+import handler from "./api/hello";
+
+type User = {
+	name: {
+		title: string;
+		first: string;
+		last: string;
+	};
+	email: string;
+	city: string;
+};
+
 export default function Home() {
-	return <div className="text-2xl"> Hello World</div>;
+	const [userData, setUserData] = useState<User[] | undefined>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		const getUser = async () => await handler();
+		getUser().then((returnedUser) => {
+			if (!returnedUser) {
+				setLoading(true);
+			} else {
+				console.log(returnedUser);
+				setUserData(returnedUser);
+				setLoading(false);
+			}
+		});
+	}, []);
+	const displayUser = () => {
+		return (
+			<div>
+				{loading ? (
+					<h1>Loading...</h1>
+				) : (
+					userData &&
+					userData.map((user, index) => (
+						<div key={index}>
+							<h1>
+								{user.name.first} {user.name.last}
+							</h1>
+							<p>{user.email}</p>
+							<p>{user.city}</p>
+						</div>
+					))
+				)}{" "}
+			</div>
+		);
+	};
+
+	return (
+		<div className="text-2xl">
+			<h1>Hello World</h1>
+			<div>{displayUser()}</div>
+		</div>
+	);
 }
